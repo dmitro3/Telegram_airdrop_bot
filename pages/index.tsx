0737da646f9498225702534dcd779dc9c875fa39
Letty-nooelussa <nooelussa@gmail.com>
2024-08-10 23:19:54 +0900

    fix: test the game

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,43 +67,43 @@ const Index: React.FC<IndexProps> = ({ data }) => {
     }, 500);
   };
   const getLevelInfo = () => {
-    switch (true) {
-      case count >= 1000000000:
-        return {
-          text: "Legendary",
-          number: 10,
-          image: "/images/lvl-10-legendary.png",
-        };
-      case count >= 100000000:
-        return { text: "Lord", number: 9, image: "/images/lvl-9-lord.png" };
-      case count >= 50000000:
-        return {
-          text: "Grand Master",
-          number: 8,
-          image: "/images/lvl-8-grand-master.png",
-        };
-      case count >= 10000000:
-        return { text: "Master", number: 7, image: "/images/lvl-7-master.png" };
-      case count >= 2000000:
-        return {
-          text: "Diamond",
-          number: 6,
-          image: "/images/lvl-6-diamond.png",
-        };
-      case count >= 1000000:
+    switch (Math.floor(count / 20)) {
+      case 0:
+        return { text: "Rookie", number: 1, image: "/images/lvl-1-rookie.png" };
+      case 1:
+        return { text: "Bronze", number: 2, image: "/images/lvl-2-bronze.png" };
+      case 2:
+        return { text: "Silver", number: 3, image: "/images/lvl-3-silver.png" };
+      case 3:
+        return { text: "Gold", number: 4, image: "/images/lvl-4-gold.png" };
+      case 4:
         return {
           text: "Platinum",
           number: 5,
           image: "/images/lvl-5-platinum.png",
         };
-      case count >= 100000:
-        return { text: "Gold", number: 4, image: "/images/lvl-4-gold.png" };
-      case count >= 25000:
-        return { text: "Silver", number: 3, image: "/images/lvl-3-silver.png" };
-      case count >= 5000:
-        return { text: "Bronze", number: 2, image: "/images/lvl-2-bronze.png" };
+      case 5:
+        return {
+          text: "Diamond",
+          number: 6,
+          image: "/images/lvl-6-diamond.png",
+        };
+      case 6:
+        return { text: "Master", number: 7, image: "/images/lvl-7-master.png" };
+      case 7:
+        return {
+          text: "Grand Master",
+          number: 8,
+          image: "/images/lvl-8-grand-master.png",
+        };
+      case 8:
+        return { text: "Lord", number: 9, image: "/images/lvl-9-lord.png" };
       default:
-        return { text: "Rookie", number: 1, image: "/images/lvl-1-rookie.png" };
+        return {
+          text: "Legendary",
+          number: 10,
+          image: "/images/lvl-10-legendary.png",
+        };
     }
   };
   const handleIncrement = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -313,14 +313,13 @@ const Index: React.FC<IndexProps> = ({ data }) => {
               </span>
               <img src="/images/pajamas_information-o.png" alt="" />
             </div>
-            <Link href="/flappy">
-              <div
-                className={`flex items-center space-x-2 bg-gradient-to-b from-[#EEEEEE] to-[#FFFFFF] shadow-[0px_4px_0px_0px_#CACACA] px-[15px] py-[10px] rounded-[10px] ml-auto ${
-                  tap > 4 ? "block" : "hidden"
-                }`}
-              >
-                <img src="/images/redbird-small.svg" alt="" />
-                <span className="text-[#E3310B]">Game Go</span>
+            <Link href="/flappygame">
+              <div className="flex items-center space-x-2 bg-gradient-to-b from-[#EEEEEE] to-[#FFFFFF] shadow-[0px_4px_0px_0px_#CACACA] px-[15px] py-[10px] rounded-[10px]">
+                <img src="/images/lightening.svg" />
+                <span>
+                  <span className="text-main">{mount}</span> / 1000
+                </span>
+                <img src="/images/pajamas_information-o.png" alt="" />
               </div>
             </Link>
           </div>
